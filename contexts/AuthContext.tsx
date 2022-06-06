6e2a4d8ed9e9c56cbc29cf763e997d9049b168da
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-06-06 19:37:44 -0300

    Broadcast de logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -21,7 +21,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-  signIn(credentials: SignInCredentials): Promise<void>
+  signIn: (credentials: SignInCredentials) => Promise<void>
+  signOut: () => void
   isAuthenticated: boolean
   user: User | undefined
 }
@@ -32,9 +33,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function signOut() {
+let authChannel: BroadcastChannel
+
+export function signOut(isBroadcasting = true) {
   destroyCookie(undefined, 'nextauth.token')
   destroyCookie(undefined, 'nextauth.refreshToken')
+
+  if (isBroadcasting) authChannel.postMessage('signOut')
 
   Router.push('/')
 }
@@ -42,6 +47,23 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel('auth')
+
+    authChannel.onmessage = message => {
+      switch (message.data) {
+        case 'signOut':
+          signOut(false)
+          break
+        // case 'signIn':
+        //   Router.push('/dashboard')
+        //   break
+        default:
+          break
+      }
+    }
+  }, [])
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
@@ -87,13 +109,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
       api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
+
+      // authChannel.postMessage('signIn')
     } catch (error) {
       console.log(error)
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user, signOut }}>
       {children}
     </AuthContext.Provider>
   )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,7 +8,7 @@ import { api } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export const Dashboard: NextPage = () => {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
 
   useEffect(() => {
     api
@@ -20,6 +20,10 @@ export const Dashboard: NextPage = () => {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button type="button" onClick={signOut}>
+        Sign out
+      </button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
